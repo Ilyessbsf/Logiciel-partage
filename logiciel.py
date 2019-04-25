a447de5c868524349dfd8e6757aa91c221acaae7
Ilyessbsf <46977150+Ilyessbsf@users.noreply.github.com>
2019-04-25 20:57:36 +0200

    test ça

--- logiciel.py
+++ logiciel.py
@@ -1,9 +1,12 @@
-#coding utf-8
+#coding: utf-8
 
-from tkinter import * 
+from Tkinter import * 
 
-HEIGHT = 500
-WIDTH = 500
+
+	
+
+HEIGHT = 600
+WIDTH = 800
 
 root = Tk()
 
@@ -12,30 +15,46 @@ canvas = Canvas(root, height = HEIGHT, width = WIDTH )
 canvas.pack()
 
 #Frame permet de créer des frames a l'interieur d'une fenetre
-Frame = Frame(root, bg = 'pink', bd = 5)
-Frame.place(relx = 0.5 , rely = 0.1 , relwidth= 0.75, relheight = 0.1 , anchor='n')
+Framefirst = Frame(root, bg = 'pink', bd = 5)
+Framefirst.place(relx = 0.5 , rely = 0.1 , relwidth= 0.75, relheight = 0.1 , anchor='n')
 
 #Titre et saisi de l'identifiant 
-IDlab = Label(Frame,text = 'Identifiant',bg='pink' ,fg = 'black')
+IDlab = Label(Framefirst,text = 'Identifiant',bg='pink' ,fg = 'black')
 IDlab.place(relx = 0.048, rely = 0.1 , relwidth= 0.2, relheight = 0.4)
 
-IdEntry = Entry(Frame, text = 'ex : Paula' , fg = 'black')
+IdEntry = Entry(Framefirst, text = 'ex : Paula' , fg = 'black')
 IdEntry.place(relx = 0.01, rely = 0.5 , relwidth= 0.3, relheight = 0.3)
 
 #Titre et saisi du mot de passe
-MDPlab = Label(Frame, text= 'Mot de passe', bg='pink' ,fg = 'black')
+MDPlab = Label(Framefirst, text= 'Mot de passe', bg='pink' ,fg = 'black')
 MDPlab.place(relx = 0.4, rely = 0.1 , relwidth= 0.2, relheight = 0.4)
 
-MDPEntry = Entry(Frame)
+MDPEntry = Entry(Framefirst)
 MDPEntry.place(relx = 0.355, rely = 0.5 , relwidth= 0.3, relheight = 0.3)
 
 #Bouton entrer
-Button = Button(Frame, text = 'Connexion', font = 20)
-Button.place(relx = 0.69, relwidth= 0.3, relheight = 1)
+Button_connexion = Button(Framefirst, text = 'Connexion', font = 20)
+Button_connexion.place(relx = 0.69, relwidth= 0.3, relheight = 1)
 
 ######################    Deuxieme Frame    #######################
 
-Deuframe = Frame(root, bg = 'pink', bd = 6)
-Deuframe.place(relx = 0.5 , rely = 0.1 , relwidth= 0.75, relheight = 0.6 , anchor='n')
+Deuframe = Frame(root, bg = 'pink', bd = 6 )
+Deuframe.place(relx = 0.5 , rely = 0.255, relwidth= 0.75, relheight = 0.7 , anchor='n')
+
+#Demande l'option au client 
+
+Option = Label(Deuframe, text = "Choisissez l'option désirée :",bg = 'pink', fg = 'black', font = 20)
+Option.place(relx = 0.33, rely = 0)
+
+#Les différentes options 
+
+DownloadFill = Button(Deuframe, text = 'Download', command = Clic)
+DownloadFill.place(relx = 0.3 , rely = 0.2, relwidth= 0.2, relheight = 0.1)
+
+UploadFill = Button(Deuframe, text = 'Upload')
+UploadFill.place(relx = 0.5 , rely = 0.2, relwidth= 0.2, relheight = 0.1)
+
+#Afiichage des labels a la demande du client
+
 
 root.mainloop()
